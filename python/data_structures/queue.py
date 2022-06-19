efc8459442416a78acbf11ca5b74c3a8510a8f43
Anthony Hill <91757022+anthonymhill23@users.noreply.github.com>
2022-06-18 21:45:48 -0400

    files added code after breaking stacks and queue file

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,35 @@
+from data_structures.stack import Node
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
 class Queue:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.front = None
+        self.rear = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self, value):
+        node = Node(value)
+        if self.rear:
+            self.rear.next = node
+        self.rear = node
+        if self.front is None:
+            self.front = node
+
+    def dequeue(self):
+        if self.front is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+
+        wanted = self.front
+        self.front = self.front.next
+        wanted.next = None
+        return wanted.value
+
+    def peek(self):
+        if self.front is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+
+        return self.front.value
+
+    def is_empty(self):
+        return self.front is None
+
